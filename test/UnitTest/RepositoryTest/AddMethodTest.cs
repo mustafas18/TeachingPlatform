fed3f18616dbe94bd85dd9219ceb83f63366350d
M <M@DESKTOP-0QJT7IB>
2023-03-22 22:43:30 +0430

    Unit testing

--- test/UnitTest/RepositoryTest/AddMethodTest.cs
+++ test/UnitTest/RepositoryTest/AddMethodTest.cs
@@ -1,3 +1,7 @@
+using Core.Entities;
+using Infrastructure.Data;
+using Infrastructure.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using System.Collections.Generic;
 using Xunit.Sdk;
@@ -11,42 +15,42 @@ namespace UnitTest.RepositoryTest
         // This class mock the DbContext and DbSet, then the class call the Add method of the repository, 
         // and finally, it is verified that those methods from mock objects have been called.
         // */
-        //[Fact]
-        //public void Add_Method_DbContext_Should_Be_Called()
-        //{
+        [Fact]
+        public void Add_Method_DbContext_Should_Be_Called()
+        {
 
-        //    // Arrange
-        //    var dbContext = new Mock<AppDbContext>();
-        //    var dbSetUserMock = new Mock<DbSet<Guest>>();
-        //    dbContext.Setup(x => x.Set<Guest>()).Returns(dbSetUserMock.Object);
-        //    var guest = new Guest { Id = 1, Name = "Ol" };
-        //    var guest2 = new Guest { Id = 2, Name = "dOl" };
+            // Arrange
+            var dbContext = new Mock<AppDbContext>();
+            var dbSetCourseMock = new Mock<DbSet<Course>>();
+            dbContext.Setup(x => x.Set<Course>()).Returns(dbSetCourseMock.Object);
+            var course = new Course { Id = 1, TitleEn="Course1" };
+            var course2 = new Course { Id = 2, TitleEn = "Course2" };
 
-        //    // Act
-        //    var repository = new Repository<Guest>(dbContext.Object);
-        //    repository.Add(guest);
+            // Act
+            var repository = new Repository<Course>(dbContext.Object);
+            repository.Add(course);
 
-        //    //Assert
-        //    dbContext.Verify(x => x.Set<Guest>());
-        //}
-        //[Fact]
-        //public void Add_Method_Should_Add_Guest1_To_dbSet()
-        //{
+            //Assert
+            dbContext.Verify(x => x.Set<Course>());
+        }
+        [Fact]
+        public void Add_Method_Should_Add_Guest1_To_dbSet()
+        {
 
-        //    // Arrange
-        //    var dbContext = new Mock<AppDbContext>();
-        //    var dbSetUserMock = new Mock<DbSet<Guest>>();
-        //    dbContext.Setup(x => x.Set<Guest>()).Returns(dbSetUserMock.Object);
-        //    var guest1 = new Guest { Id = 1, Name = "Ol" };
-        //    var guest2 = new Guest { Id = 2, Name = "dOl" };
+            // Arrange
+            var dbContext = new Mock<AppDbContext>();
+            var dbSetUserMock = new Mock<DbSet<Course>>();
+            dbContext.Setup(x => x.Set<Course>()).Returns(dbSetUserMock.Object);
+            var course = new Course { Id = 1, TitleEn = "Course1" };
+            var course2 = new Course { Id = 2, TitleEn = "Course2" };
 
-        //    // Act
-        //    var repository = new Repository<Guest>(dbContext.Object);
-        //    repository.Add(guest1);
+            // Act
+            var repository = new Repository<Course>(dbContext.Object);
+            repository.Add(course);
 
-        //    //Assert
-        //    dbSetUserMock.Verify(x => x.Add(It.Is<Guest>(y => y == guest2)));
-        //}
+            //Assert
+            dbSetUserMock.Verify(x => x.Add(It.Is<Course>(y => y == course)));
+        }
 
     }
 }
